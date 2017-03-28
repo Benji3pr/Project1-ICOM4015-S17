d809de82408b3c85be5a483be91c5c2c07430673
luis <luis@AnakinSkywalker-WL>
2017-03-28 17:47:26 -0400

    less for loops in checkBombsArround

--- Lab5/src/Minesweeper.java
+++ Lab5/src/Minesweeper.java
@@ -68,108 +68,72 @@ public class Minesweeper {
 	public String checkBombsArround(int x, int y){
 		int amount = 0;
 		
+		int limitI = 0; 
+		int limitJ = 0; 
+		
 		if((x>0)&&(y>0)&&(x<8)&&(y<8)){
 			x-=1;
-			y-=1;			
-			for(int i=x; i<(x+3); i++){
-				for(int j=y; j<(y+3); j++){
-					if(checkBomb(i,j)){
-						amount+=1;
-					}
-				}
-			}
-		}
-		
-		else if(x==0 && y==0){
-			for(int i=x; i<(x+2); i++){
-				for(int j=y; j<(y+2); j++){
-					if(checkBomb(i,j)){
-						amount+=1;
-					}
-				}
-			}
-		}
-		
-		else if(x==0 && y==8){
+			y-=1;		
+			
+			limitI = (x+3); 
+			limitJ = (y+3); 
+				
+			
+		} else if(x==0 && y==0){
+			limitI = (x+2);
+			limitJ = (y+2);
+		} else if(x==0 && y==8){
 			y-=1;
-			for(int i=x; i<(x+2); i++){
-				for(int j=y; j<(y+2); j++){
-					if(checkBomb(i,j)){
-						amount+=1;
-					}
-				}
-			}
-		}
-		
-		else if(x==8 && y==0){
+			
+			limitI = (x+2);
+			limitJ = (y+2);
+		} else if(x==8 && y==0){
 			x-=1;
-			for(int i=x; i<(x+2); i++){
-				for(int j=y; j<(y+2); j++){
-					if(checkBomb(i,j)){
-						amount+=1;
-					}
-				}
-			}
-		}
-		
-		else if(x==8 && y==8){
+			
+			limitI = (x+2);
+			limitJ = (y+2);
+		} else if(x==8 && y==8){
 			x-=1;
 			y-=1;
-			for(int i=x; i<(x+2); i++){
-				for(int j=y; j<(y+2); j++){
-					if(checkBomb(i,j)){
-						amount+=1;
-					}
-				}
-			}
-		}
-		
-		else if(x==0 && y>0 && y<8){
+			
+			limitI = (x+2);
+			limitJ = (y+2);
+			
+		} else if(x==0 && y>0 && y<8){
 			y-=1;
-			for(int i=x; i<(x+2); i++){
-				for(int j=y; j<(y+3); j++){
-					if(checkBomb(i,j)){
-						amount+=1;
-					}
-				}
-			}
-		}
-		
-		else if(x==8 && y>0 && y<8){
+			
+			limitI = (x+2);
+			limitJ = (y+3);
+			
+		} else if(x==8 && y>0 && y<8){
 			y-=1;
 			x-=1;
-			for(int i=x; i<(x+2); i++){
-				for(int j=y; j<(y+3); j++){
-					if(checkBomb(i,j)){
-						amount+=1;
-					}
-				}
-			}
-		}
-		
-		else if(y==0 && x>0 && x<8){
+			
+			limitI = (x+2);
+			limitJ = (y+3);
+			
+		} else if(y==0 && x>0 && x<8){
 			x-=1;
-			for(int i=x; i<(x+3); i++){
-				for(int j=y; j<(y+2); j++){
-					if(checkBomb(i,j)){
-						amount+=1;
-					}
-				}
-			}
-		}
-		
-		else if(y==8 && x>0 && x<8){
+			
+			limitI = (x+3);
+			limitJ = (y+2);
+			
+		} else if(y==8 && x>0 && x<8){
 			x-=1;
 			y-=1;
-			for(int i=x; i<(x+3); i++){
-				for(int j=y; j<(y+2); j++){
-					if(checkBomb(i,j)){
-						amount+=1;
-					}
+			
+			limitI = (x+3);
+			limitJ = (y+2);
+					
+		}
+		
+		for(int i = x; i< limitI; i++){
+			for(int j=y; j< limitJ; j++){
+				if(checkBomb(i,j)){
+					amount+=1;
 				}
 			}
 		}
-		
 	
 		String totalAmount = Integer.toString(amount);
 		return totalAmount;
